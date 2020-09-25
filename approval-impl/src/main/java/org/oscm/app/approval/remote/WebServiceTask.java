b60ba16141828c4d661bb6f84ed63892c22eb4ec
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2020-09-25 13:32:48 +0200

    Cxf jars removal and related changes

--- approval-impl/src/main/java/org/oscm/app/approval/remote/WebServiceTask.java
+++ approval-impl/src/main/java/org/oscm/app/approval/remote/WebServiceTask.java
@@ -1,76 +1,84 @@
-/********************************************************************************
+/**
+ * ******************************************************************************
  *
- * Copyright FUJITSU LIMITED 2020
+ * <p>Copyright FUJITSU LIMITED 2020
  *
- *******************************************************************************/
+ * <p>*****************************************************************************
+ */
 package org.oscm.app.approval.remote;
 
-import java.net.MalformedURLException;
-
+import org.oscm.app.dataaccess.AppDataService;
+import org.oscm.app.v2_0.SOAPSecurityHandler;
+import org.oscm.app.v2_0.data.PasswordAuthentication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.cxf.common.util.ProxyClassLoader;
-import org.apache.cxf.frontend.ClientProxyFactoryBean;
-import org.apache.cxf.jaxws.JaxWsClientFactoryBean;
-import org.oscm.app.v2_0.BSSWebServiceFactory;
-import org.oscm.app.v2_0.data.PasswordAuthentication;
-import org.oscm.app.v2_0.exceptions.ConfigurationException;
+import javax.jws.WebService;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Binding;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.Handler;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * @author goebel
- *
- */
+/** @author goebel */
 public abstract class WebServiceTask<T> extends Thread {
-    public static String RC_OK = "OK";
-    
-    protected static final Logger logger = LoggerFactory.getLogger(WebServiceTask.class);
-    private Class<T> webService;
-    private Object[] rc = new Object[1];
+  public static String RC_OK = "OK";
 
-    private T service;
-    private final PasswordAuthentication[] pa = new PasswordAuthentication[1];
+  protected static final Logger logger = LoggerFactory.getLogger(WebServiceTask.class);
+  private Class<T> webService;
+  private Object[] rc = new Object[1];
 
-    public WebServiceTask(Class<T> ws)
-            throws MalformedURLException, ConfigurationException {
-        webService = ws;
-        this.rc[0] = null;
+  private final PasswordAuthentication[] pa = new PasswordAuthentication[1];
+
+  public WebServiceTask(Class<T> ws) {
+    webService = ws;
+    this.rc[0] = null;
+  }
+
+  Object getResult() {
+    return rc[0];
+  }
+
+  void setAuthentication(PasswordAuthentication pa) {
+    this.pa[0] = pa;
+  }
+
+  @Override
+  public void run() {
+    try {
+      T webService = loadBSSWebService();
+      rc[0] = execute(webService);
+    } catch (Exception e) {
+      rc[0] = e;
     }
-    
-    Object getResult() {
-        return rc[0];
+  }
+
+  private T loadBSSWebService() throws Exception {
+    AppDataService das = new AppDataService();
+    String webServiceWsdl = das.loadBesWebServiceWsdl();
+    webServiceWsdl = webServiceWsdl.replace("{SERVICE}", webService.getSimpleName());
+
+    String targetNamespace = webService.getAnnotation(WebService.class).targetNamespace();
+    QName serviceQName = new QName(targetNamespace, webService.getSimpleName());
+    Service serviceService = Service.create(new URL(webServiceWsdl), serviceQName);
+    T port = serviceService.getPort(webService);
+
+    BindingProvider bindingProvider = (BindingProvider) port;
+
+    Binding binding = bindingProvider.getBinding();
+    List<Handler> handlerChain = binding.getHandlerChain();
+    if (handlerChain == null) {
+      handlerChain = new ArrayList<>();
     }
 
-    void setAuthentication(PasswordAuthentication pa) {
-        this.pa[0] = pa;
-    }
+    handlerChain.add(new SOAPSecurityHandler(pa[0].getUserName(), pa[0].getPassword()));
+    binding.setHandlerChain(handlerChain);
 
-    @Override
-    public void run() {
-        JaxWsClientFactoryBean fb = new JaxWsClientFactoryBean();
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        try {
-            ProxyClassLoader pcl = new ProxyClassLoader(webService.getClassLoader());
-            pcl.addLoader(cl);
-            pcl.addLoader(ClientProxyFactoryBean.class.getClassLoader());
-            switchClassLoader(pcl);
+    return port;
+  }
 
-            service = BSSWebServiceFactory.getBSSWebService(webService, pa[0]);
-
-            rc[0] = execute(service);
-
-        } catch (Exception e) {
-            rc[0] = e;
-        } finally {
-            switchClassLoader(cl);
-        }
-    }
-
-    private void switchClassLoader(ClassLoader cl) {
-        logger.info(String.format("Setting class loader %s", cl.getClass().getName()));
-        Thread.currentThread().setContextClassLoader(cl);
-    }
-
-    public abstract Object execute(T service) throws Exception;
-
+  public abstract Object execute(T service) throws Exception;
 }
